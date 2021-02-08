4eb04648d863963d3657a231b99bfe2e217c5f76
Michael Leonhard <77648651+MSLeonhard@users.noreply.github.com>
2021-02-08 10:56:13 -0500

    completed step 2 of activity 1

--- js/grades.js
+++ js/grades.js
@@ -1,1 +1,29 @@
 console.log('connected...')
+
+let grades = [
+    68, 
+    49,
+    58,
+    75,
+    96,
+    97,
+    97,
+    68,
+    46,
+    54,
+    82,
+    93,	
+    51,
+    71,
+    82,
+    91,
+    73,
+    68,
+    83,
+    74
+]
+
+for ( let i = 0; i < grades.length; i++) {
+    document.getElementById('output').innerHTML += `${grades[i]}<br>`
+}
+document.getElementById('output2').innerHTML = `The number of grades is ${grades.length}`
