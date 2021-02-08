82c08b0c7beb1bd0d4ccf4080dda10cedbb1882b
Michael Leonhard <77648651+MSLeonhard@users.noreply.github.com>
2021-02-08 11:52:02 -0500

    completed activity 1

--- js/grades.js
+++ js/grades.js
@@ -7,7 +7,7 @@ let grades = [
     75,
     96,
     97,
-    97,
+    98,
     68,
     46,
     54,
@@ -23,7 +23,33 @@ let grades = [
     74
 ]
 
+let main = `The number of grades is ${grades.length}<br>`
+
 for ( let i = 0; i < grades.length; i++) {
-    document.getElementById('output').innerHTML += `${grades[i]}<br>`
+    main += `Grade ${i+1}: ${grades[i]}<br>`
 }
-document.getElementById('output2').innerHTML = `The number of grades is ${grades.length}`
+
+let total = 0;
+for(let i = 0; i < grades.length; i++) {
+    total += grades[i];
+}
+let avg = total / grades.length;
+
+grades.sort()
+let lowMiddle = Math.floor((grades.length - 1) / 2);
+let highMiddle = Math.ceil((grades.length - 1) / 2);
+let median = (grades[lowMiddle] + grades[highMiddle]) / 2;
+
+let over70 = []
+
+for ( let j = 0; j < grades.length; j++) {
+    if (grades[j] >= 70){
+        over70.push(grades[j])
+    }
+}
+
+main += `The highest grade was ${Math.max(...grades)}<br> The lowest grade was ${Math.min(...grades)}<br> The mean grade is ${avg}<br> The median grade is ${median} <br> ${over70.length} people got over 70% on the test`
+
+document.getElementById('output').innerHTML = main
+
+//console.log(Math.max.apply(Math, grades))
